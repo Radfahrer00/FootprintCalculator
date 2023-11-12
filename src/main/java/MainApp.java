290734f9d8b33a7fea7edc0e1b669f7ddb12a596
Quentin Mathieu <mathieu@cip.ifi.lmu.de>
2023-11-12 11:44:23 +0100

    Calculate footprint based on input

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -41,11 +41,12 @@ public class MainApp {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel mainPanel = new JPanel(new BorderLayout());
-        mainPanel.setPreferredSize(new Dimension(400, 200));
+        mainPanel.setPreferredSize(new Dimension(550, 440));
 
         // Panel for displaying global average
-        JPanel globalAveragePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        JLabel globalAverageLabel = new JLabel("  Global average for today:");
+        JPanel globalAveragePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        globalAveragePanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0)); // Add vertical space
+        JLabel globalAverageLabel = new JLabel("Global average for today:");
         globalAverageField = new JTextField(10);
         globalAverageField.setEditable(false);
         globalAveragePanel.add(globalAverageLabel);
@@ -57,7 +58,7 @@ public class MainApp {
         headerLabel.setHorizontalAlignment(SwingConstants.CENTER);
         mainPanel.add(headerLabel, BorderLayout.CENTER);
 
-        JPanel inputPanel = new JPanel(new GridLayout(6, 2));
+        JPanel inputPanel = new JPanel(new GridLayout(11, 2));
         placeComponents(inputPanel);
 
         mainPanel.add(inputPanel, BorderLayout.SOUTH);
@@ -69,30 +70,55 @@ public class MainApp {
 
     private static void placeComponents(JPanel panel) {
 
-        JLabel label1 = new JLabel("    Value 1:");
+        JLabel label1 = new JLabel("Beef (in grams):");
         JTextField value1Field = new JTextField(10);
         panel.add(label1);
         panel.add(value1Field);
 
-        JLabel label2 = new JLabel("    Value 2:");
+        JLabel label2 = new JLabel("Pork (in gram):");
         JTextField value2Field = new JTextField(10);
         panel.add(label2);
         panel.add(value2Field);
 
-        JLabel label3 = new JLabel("    Value 3:");
+        JLabel label3 = new JLabel("Chicken (in gram):");
         JTextField value3Field = new JTextField(10);
         panel.add(label3);
         panel.add(value3Field);
 
-        JLabel label4 = new JLabel("    Value 4:");
+        JLabel label4 = new JLabel("Fish (in gram):");
         JTextField value4Field = new JTextField(10);
         panel.add(label4);
         panel.add(value4Field);
 
-        JLabel label5 = new JLabel("    Value 5:");
+        JLabel label5 = new JLabel("Butter (in gram):");
         JTextField value5Field = new JTextField(10);
         panel.add(label5);
         panel.add(value5Field);
+
+        JLabel label6 = new JLabel("Other dairy products (in gram):");
+        JTextField value6Field = new JTextField(10);
+        panel.add(label6);
+        panel.add(value6Field);
+
+        JLabel label7 = new JLabel("Car (in km):");
+        JTextField value7Field = new JTextField(10);
+        panel.add(label7);
+        panel.add(value7Field);
+
+        JLabel label8 = new JLabel("Public transport (in km):");
+        JTextField value8Field = new JTextField(10);
+        panel.add(label8);
+        panel.add(value8Field);
+
+        JLabel label9 = new JLabel("Plane (in km):");
+        JTextField value9Field = new JTextField(10);
+        panel.add(label9);
+        panel.add(value9Field);
+
+        JLabel label10 = new JLabel("<html>Electrical appliances,<br>e.g. washing machine (in number of uses):</html>");
+        JTextField value10Field = new JTextField(10);
+        panel.add(label10);
+        panel.add(value10Field);
 
         JButton submitButton = new JButton("Submit");
         panel.add(submitButton);
@@ -100,19 +126,25 @@ public class MainApp {
         submitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                int value1 = Integer.parseInt(value1Field.getText());
-                int value2 = Integer.parseInt(value2Field.getText());
-                int value3 = Integer.parseInt(value3Field.getText());
-                int value4 = Integer.parseInt(value4Field.getText());
-                int value5 = Integer.parseInt(value5Field.getText());
-                int result = value1 + value2 + value3 + value4 + value5;
+                int beef = Integer.parseInt(value1Field.getText());
+                int pork = Integer.parseInt(value2Field.getText());
+                int chicken = Integer.parseInt(value3Field.getText());
+                int fish = Integer.parseInt(value4Field.getText());
+                int butter = Integer.parseInt(value5Field.getText());
+                int dairyProducts = Integer.parseInt(value6Field.getText());
+                int car = Integer.parseInt(value7Field.getText());
+                int pTransport = Integer.parseInt(value8Field.getText());
+                int plane = Integer.parseInt(value9Field.getText());
+                int electricalAppliances = Integer.parseInt(value10Field.getText());
+
+                float result = calculateConsumption(beef, pork, chicken, fish, butter, dairyProducts, car,
+                        pTransport, plane, electricalAppliances);
 
                 String messageToSend = getCurrentTime() + "_" + result;
                 System.out.println("Message: " + messageToSend);
 
 
-                // TODO: Send result to MQTT broker
-                // Publish the result to another topic
+                // Publish the result to topic
                 MqttMessage mqttMessage = new MqttMessage(messageToSend.getBytes());
                 try {
                     mqttClient.publish(TOPIC_PUBLISH, mqttMessage);
@@ -120,7 +152,25 @@ public class MainApp {
                     mqttException.printStackTrace();
                 }
 
-                JOptionPane.showMessageDialog(panel, "Sum: " + result + "\n" + " Message to send: " + messageToSend, "Result", JOptionPane.INFORMATION_MESSAGE);
+                String averageComparison = "The average for today is: ";
+                if (!globalAverageField.getText().isEmpty()) {
+                    // Convert the string to a floating-point number
+                    double averageValue = Double.parseDouble(globalAverageField.getText());
+                    if (result < averageValue) {
+                        averageComparison += averageValue + ". You have consumed less than the average. Nice!";
+                    } else if (result > averageValue) {
+                        averageComparison += averageValue + ". You have consumed more than the average.\n" +
+                                "Try to lower your consumption!";
+                    } else {
+                        averageComparison += averageValue + ". You have consumed exactly the average.";
+                    }
+                } else {
+                    averageComparison = "You are the first to publish your data for the day.\n" +
+                            "Come back later to compare yourself to the average.";
+                }
+
+                JOptionPane.showMessageDialog(panel, "Sum: " + result + "\n" +
+                        averageComparison, "Result", JOptionPane.INFORMATION_MESSAGE);
             }
         });
     }
@@ -135,7 +185,30 @@ public class MainApp {
     // Method to update the global average field with the incoming message
     private static void updateGlobalAverageField(MqttMessage mqttMessage) {
         String incomingMessage = new String(mqttMessage.getPayload());
-        SwingUtilities.invokeLater(() -> globalAverageField.setText(incomingMessage));
+        // Convert the string to a floating-point number
+        double incomingValue = Double.parseDouble(incomingMessage);
+        // Format the number to a string with two decimal places
+        String formattedValue = String.format("%.2f", incomingValue);
+        SwingUtilities.invokeLater(() -> globalAverageField.setText(formattedValue));
+    }
+
+    private static float calculateConsumption(int beef, int pork, int chicken, int fish, int butter, int dairyProducts, int carKm,
+                                              int pTransportKm, int planeKm, int electricalAppliances) {
+        float beefConsumption = (float) (beef * 16.88);
+        float porkConsumption = (float) (pork * 6.92);
+        float chickenConsumption = (float) (chicken * 2.79);
+        float fishConsumption = (float) (fish * 5.14);
+        float butterConsumption = (float) (butter * 12.11);
+        float dairyProductsConsumption = (float) (dairyProducts * 5.89);
+        float carConsumption = carKm * 171;
+        float pTransportConsumption = pTransportKm * 67;
+        float planeConsumption = planeKm * 365;
+        float appliancesConsumption = electricalAppliances * 750;
+
+        float totalConsumption = beefConsumption + porkConsumption + chickenConsumption + fishConsumption + butterConsumption
+                + dairyProductsConsumption + carConsumption + pTransportConsumption + planeConsumption + appliancesConsumption;
+        return totalConsumption;
     }
 }
 
+// Source for CO2 Consumption Values: https://interaktiv.tagesspiegel.de/lab/wie-klimaschaedlich-sind-beliebte-lebensmittel/
